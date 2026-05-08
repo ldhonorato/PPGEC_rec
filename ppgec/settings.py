2540eba25184bc9be508df7740df817967f0154b
DaveOlivae <davioliveiramelo2604@gmail.com>
2026-05-08 18:59:29 -0300

    fluxo de envio de emails apos abertura de processo implementado

--- ppgec/settings.py
+++ ppgec/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load environment variables from .env
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -137,9 +141,9 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"          # ou o SMTP do teu provedor
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "acadflow@ecomp.poli.br"
-EMAIL_HOST_PASSWORD = "sua-senha-aqui"  # use variável de ambiente em produção
-DEFAULT_FROM_EMAIL = "AcadFlow PPGEC <acadflow@ecomp.poli.br>"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "")
 
 # celery config
 CELERY_BROKER_URL = "redis://localhost:6379/0"
@@ -147,5 +151,5 @@ CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 
-# django-celery-results
+# django-celery-results 
 INSTALLED_APPS += ["django_celery_results"]
