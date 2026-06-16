851e2d60405758a4d0635c7633c3b384790d445b
Hayato420 <mjsa@poli.br>
2026-06-15 21:03:47 -0300

    fix: corrige acesso às variáveis de ambiente em 'settings.py'

--- ppgec/settings.py
+++ ppgec/settings.py
@@ -201,14 +201,14 @@ LOGOUT_REDIRECT_URL = 'login'
 # email config
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
 
-EMAIL_HOST_USER = "EMAIL"
-EMAIL_HOST_PASSWORD = "SENHA DE APP DO EMAIL"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
-DEFAULT_FROM_EMAIL = "AcadFlow <ellenbaronebea@gmail.com>"
+DEFAULT_FROM_EMAIL = f"AcadFlow <{EMAIL_HOST_USER}>"
 
 SITE_URL = os.getenv("SITE_URL", "http://localhost:8000")
 
