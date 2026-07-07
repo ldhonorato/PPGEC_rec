c7582abbc2da3666b51e801793526182976ea692
Leandro Honorato <lhss@ecomp.poli.br>
2026-07-07 08:49:41 -0300

    Removendo comentário no settings

--- ppgec/settings.py
+++ ppgec/settings.py
@@ -213,7 +213,7 @@ SITE_URL = os.getenv("SITE_URL", "http://localhost:8000")
 
 # celery config
 CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://redis:6379/0")
-CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://redis:6379/0") (tirar comentario dessa linha)
+#CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://redis:6379/0") (tirar comentario dessa linha)
 
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
