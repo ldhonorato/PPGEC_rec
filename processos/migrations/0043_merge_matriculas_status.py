1e7406c537aa8caea45f318832d307cf7fd66d5a
Leandro Honorato <ld.honorato@gmail.com>
2026-07-09 10:09:39 -0300

    Correção da falta de imports

---
+++ processos/migrations/0043_merge_matriculas_status.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("processos", "0038_periodoletivo_status"),
+        ("processos", "0042_merge_20260707_2111"),
+    ]
+
+    operations = []
